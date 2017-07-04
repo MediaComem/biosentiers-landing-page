431ffc68497211b4f91f234a3eb7d8424bbd2603
Mathias Oberson <mathias.oberson@heig-vd.ch>
2017-07-04 09:51:03 +0200

    Initial Commit

---
+++ js/biosentiers.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+  var $apropos = $("#apropos");
+  var $panels = $("div.panels", $apropos);
+  var $images = $("div.images", $apropos);
+
+  /* ----- EVENTS ----- */
+  $("div.slice").click(selectSlice);
+
+  /**
+   * Unselect the given slice in the "À propos" section.
+   * This means hidding its content and collapsing the slice.
+   * @param {*} slice 
+   */
+  function unselectSlice($slice) {
+    $slice.removeClass('selected');
+    $('div.content', $slice).addClass('hidden');
+  }
+
+  /**
+   * Select a clicked slice in the "À propos" section.
+   */
+  function selectSlice() {
+    var $slice = $(this);
+    var $shown = $('img:not(.hidden)', $images);
+
+    // If the user clicked an already opened slice, it will close iteself
+    if ($slice.hasClass('selected')) {
+      unselectSlice($slice);
+      // Show the default image
+      $('#aboutDef').removeClass('hidden');
+    } else {
+      var $selected = $panels.children('div.selected')
+      unselectSlice($selected);
+      $slice.addClass('selected');
+      // Show the adequate image depending on the slice clicked.
+      $slice.hasClass('red') && $("#about1").removeClass('hidden');
+      $slice.hasClass('orange') && $("#about2").removeClass('hidden');
+      $slice.hasClass('blue') && $("#about3").removeClass('hidden');
+      // Wait for the slice to be completely open before showing its content.
+      setTimeout(function () {
+        $('div.content', $slice).removeClass('hidden');
+      }, 300); // The 300 here corresponds to the 0.3s of transition in the CSS
+    }
+    $shown.addClass('hidden');
+  }
+});
---
+++ js/script.js
@@ -0,0 +1,43 @@
+$( document ).ready( function() {
+  $('.panel').click( function() {
+    var imgs = $('.img-accord');
+    for(i=0;i < imgs.length;i++){
+      imgs[i].style.visibility='hidden';
+    }
+
+    $("#"+$(this).attr("target-id")).css("visibility", "visible")
+
+    var currentItemClass = this.className;
+
+    var child = $(this).children(".panel-heading1");
+    var Expandables = ($(child).attr("aria-expanded") == "true");
+    console.log("1",Expandables, typeof(Expandables));
+
+    var panels = $('.panel');
+
+    for (var i = 0; i < panels.length; i++) {
+      var className = panels[i].className;
+      var child = $(panels[i]).children(".panel-heading1");
+      if(currentItemClass == className){
+        child.css({
+          "padding-top": "10%",
+          "padding-bottom": "10%",
+        })
+      } else {
+        if(Expandables) {
+          console.log("5%", Expandables);
+          child.css({
+            "padding-top": "5%",
+            "padding-bottom": "5%",
+          })
+        } else {
+          console.log("10%", Expandables);
+          child.css({
+            "padding-top": "10%",
+            "padding-bottom": "10%",
+          })
+        }
+      }
+    }
+  })
+});
